65c1ccd3fbca5c0b6b9884d9f17af08cbdb529d3
Nancy-GDA <nancy.glez.dea@gmail.com>
2022-09-01 23:48:38 -0500

    Logica de rebancha e inicio de contador y btn reinicio

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const selectionSection = document.querySelector('#seleccion')
 let boxPlayer
 let boxPC
 
+
+//Objet with name,selector id and image path
 const figure = [
     {name:'Piedra',id:'btn-rock', src: 'image/piedra.png'},
     {name:'Papel',id:'btn-paper', src: 'image/papel.png'},
@@ -19,6 +21,7 @@ for (const element of selectionSection.children) {
     })
 }
 
+//function that randomly draws a figure
 const getComputerOption = () =>{
     let figureRandom = Math.floor(Math.random()* figure.length)
     return figure[figureRandom]
@@ -27,6 +30,7 @@ const getComputerOption = () =>{
 const pcRandom = (player)=>{
     const turnPC = getComputerOption()
     const boxBattle = document.querySelectorAll('.box-battle')
+    const marker = document.querySelector('.marker')
     boxBattle[0].classList.add('spinner')
     boxBattle[2].classList.add('spinner')
 
@@ -34,30 +38,62 @@ const pcRandom = (player)=>{
         boxBattle[0].classList.remove('spinner')
         boxBattle[2].classList.remove('spinner')
 
-        boxBattle[0].children[0].src = turnPC.src
         boxBattle[2].children[0].src = player.src
+        boxBattle[0].children[0].src = turnPC.src
+        
     }, 500)
 
     battlefield.style.display = 'flex'
+    marker.style.display = 'flex'
     startGame(player.name, turnPC.name)
 }
 
 const startGame = (player, turnPC)=>{
-    console.log(figure.name)
+    const restart = document.querySelector('.restart')
+
     if(player == turnPC){
         textMessage.innerText = 'Empate'
-    }else if(player == figure.name && turnPC == figure.name){
+
+    }else if(player == 'Piedra' && turnPC == 'Tijera'){
         textMessage.innerText = 'Ganaste 🥳'
-    }else if(player == figure.name && turnPC == figure.name){
+    }else if(player == 'Papel' && turnPC == 'Piedra'){
         textMessage.innerText = 'Ganaste 🥳'
-    }else if (player == figure.name && turnPC ==figure.name){
+    }else if (player == 'Tijera' && turnPC == 'Papel'){
         textMessage.innerText = 'Ganaste 🥳'
     }else{
-        textMessage.innerText = 'Perdiste ga tu madre 😔'
+        textMessage.innerText = 'Perdiste 😔'
     }
 
+    restart.style.display = 'flex'
+    restart.addEventListener('click',restart)
     
 }
+resultFinallyPlayer =document.querySelector('#result-player')
+
+    
+    
+  const courserPC = () =>{
+    let livesPc = 5
+    resultFinallyPc =document.querySelector('#result-PC')
+    for(let i =0;i<livesPc;i++){
+     console.log(`soy ${resultFinallyPc}`)
+    }
+  }
+  courserPC()
+
+
+const restart = () =>{
+    location.reload()
+}
+
+
+
+
+
+window.addEventListener('load',startGame)
+    
+
+
 
 
 
